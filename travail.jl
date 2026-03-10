ad95e6f245f88119d632d3cbacdff1e2889f4dec
Christina Nadler <christina.nadler@umontreal.ca>
2026-03-10 12:57:59 -0400

    ajout du début du code

--- travail.jl
+++ travail.jl
@@ -2,9 +2,9 @@
 # title: Titre du travail
 # repository: tpoisot/BIO245-modele
 # auteurs:
-#    - nom: Auteur
-#      prenom: Premier
-#      matricule: XXXXXXXX
+#    - nom: Nadler
+#      prenom: Christina
+#      matricule: 20313890
 #      github: premierAuteur
 #    - nom: Auteur
 #      prenom: Deuxième
@@ -23,10 +23,83 @@
 import Random
 Random.seed!(123456)
 using CairoMakie
+using Distributions
 
 # ## Une autre section
+"""
+    check_transition_matrix!(T)
+
+Cette fonction vérifie que chaque ligne de la matrice de transition somme à 1. Si ce n'est pas le cas, elle normalise la ligne en divisant chaque valeur par la somme de la ligne.
+"""
+function check_transition_matrix!(T)
+    for ligne in axes(T, 1)
+        if sum(T[ligne, :]) != 1
+            @warn "La somme de la ligne $(ligne) n'est pas égale à 1 et a été modifiée"
+            T[ligne, :] ./= sum(T[ligne, :])
+        end
+    end
+    return T
+end
 
 """
+    check_function_arguments(transitions, states)
+
+Cette fonction vérifie que les dimensions des arguments sont valides: la matrice de transition doit être carrée et le nombre d'états doit correspondre au nombre de lignes de la matrice
+"""
+
+function check_function_arguments(transitions, states)
+    if size(transitions, 1) != size(transitions, 2)
+        throw("Le nombre d'états ne correspond pas à la matrice de transition")
+    end
+    return nothing
+end
+
+"""
+    _sim_stochastic!(timeseries, transitions, generation)
+
+Cette fonction simule une transition stochastique entre les états en utilisant les probabilités de la matrice
+"""
+function _sim_stochastic!(timeseries, transitions, generation)
+    for state in axes(timeseries, 1)
+        pop_change = rand(Multinomial(timeseries[state, generation], transitions[state, :]))
+        timeseries[:, generation+1] .+= pop_change
+    end
+end
+
+"""
+    _sim_deterministe!(timeseries, transitions, generation)
+
+Cette fonction simule une transition déterministe en multipliant le vecteur d'états par la matrice de transition
+"""
+function _sim_deterministe!(timeseries, transitions, generation)
+    pop_change = (timeseries[:, generation]' * transitions)'
+    timeseries[:, generation+1] .+= pop_change
+end
+
+"""
+    simulation(transitions, states)
+
+Cette fonction est celle principale qui exécute le simulation du modèle de transition sur plusieurs générations
+"""
+function simulation(transitions, states; generations = 500, stochastic = false)
+    check_transition_matrix!(transitions)
+    check_function_arguments(transitions, states)
+
+    _data_type = stochastic ? Int64 : Float32
+    timeseries = zeros(_data_type, length(states), generations + 1)
+    timeseries[:, 1] = states
+
+    _sim_function! = stochastic ? _sim_stochastic! : _sim_deterministe!
+
+    for generation in Base.OneTo(generations)
+        _sim_function!(timeseries, transitions, generation)
+    end
+    return timeseries
+end
+
+
+
+    """
     foo(x, y)
 
 Cette fonction ne fait rien.
