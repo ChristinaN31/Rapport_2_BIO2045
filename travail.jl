2425ad12744af4f083a57bd84200e83718bd4a7a
Christina Nadler <christina.nadler@umontreal.ca>
2026-03-10 14:30:11 -0400

    ajout de la deuxième moité du code

--- travail.jl
+++ travail.jl
@@ -109,6 +109,58 @@ function foo(x, y)
     return nothing
 end
 
+# Les états du paysage:
+# Il y a 4 états: vide, herbes, buisson_A, buisson_B
+# Il y a 200 parcelles, toutes initiallement vides mais on peut planter jusqu'à 50 parcelles de buissons
+# on peut donc dire qu'on va planter 35 parcelles de buisson_A et 15 parcelles de buisson_B
+# alors il y a 150 vides, 0 herbes, 25 buissons_A et 15 buissons_B
+
+# États
+# Vide, Herbes, Buissons_A, buissons_B
+s = [150, 0, 35, 15] #note qu'à l'équilibre, on veut que seulement 20% des parcelles soient végétalisées... ça veut dire que les parcelles vides doivent rester stables et les transitions vers les parcelles végétalisées doivent être limitées  
+states = length(s)
+patches = sum(s)
+
+# création de la matrice de transition
+T = zeros(Float64, states, states)
+T[1, :] = [360, 15, 15, 10] #transitions par vide
+# ces valeurs vont devenir des probabilités plus tard, et donc on a choisi d'utiliser des gros chiffres pour assurer qu'à l'équilibre, seulement 20% des parcelles sont végétalisées
+# la somme de toutes les parcelles végétalisées est 40 (15+15+19)
+# parmi ces 20%, 30$ doivent être des parcelles herbes
+# 0.30 * 40 = 12 
+T[2, :] = [80, 280, 25, 15] #transition par herbes
+# les parcelles herbes restent majoritairement herbes mais peuvent aussi changer 
+# parmi ces 20%, 70% sont des buissons (un type de buisson doit être plus abdondant que l'autre)
+# 0.70 * 40 = 28
+# on peut dire que il y a 18 parcelles de Buissons_A et 10 Buissons_B
+T[3, :] = [40, 15, 320, 25] #transition par buisson_A 
+T[4, :] = [40, 15, 25, 320] #transition par buisson_B
+
+states_names = ["Vide", "Herbes", "Buissons A", "Buissons B"]
+states_colors = [:grey40, :orange, :teal, :purple]
+
+# Simulations
+
+f = Figure()
+ax = Axis(f[1, 1], xlabel="Nb. générations", ylabel="Nb. parcelles")
+#simulation stochastique
+for _ in 1:100
+    sto_sim = simulation(T, s; stochastic = true, generations = 200)
+    for i in eachindex(s)
+        lines!(ax, sto_sim[i, :], color=states_colors[i], alpha = 0.1)
+    end
+end
+#simulation déterministe
+det_sim = simulation(T, s; stochastic = false, generations = 200)
+for i in eachindex(s)
+    lines!(ax, det_sim[i, :], color=states_colors[i], alpha = 1,
+    label=states_names[i], linewidth=4)
+end
+
+axislegend(ax)
+tightlimits!(ax)
+current_figure()
+
 # # Présentation des résultats
 
 # La figure suivante représente des valeurs aléatoires:
